d1a27bfc7b8b3fd49e60417f83f3d570390f0839
Anthony K. Mwangi <68157384+raskovnik@users.noreply.github.com>
2023-02-07 16:14:20 +0300

    Handling CORS errors

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
+//middleware
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Acess-Control-Allow-Headers", "*");
+
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next(); 
+})
+
+//routes
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
