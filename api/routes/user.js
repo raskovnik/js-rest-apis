00452012babf236e0dc7230ac6b125db0a27a6e1
Anthony K. Mwangi <68157384+raskovnik@users.noreply.github.com>
2023-02-11 21:49:02 +0300

    user signup

---
+++ api/routes/user.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+router.post("/signup", (req, res, next) => {
+    User.find({email: req.body.email})
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "Mail exists"
+                })
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        })
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: "User Created"
+                                })
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                })
+                            });
+                    }
+                });
+            }
+        });
+});
+
+router.get("/", (req, res, next) => {
+    User.find()
+    .exec()
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            users: docs.map(doc => {
+                return {
+                    email: doc.email,
+                    userId: doc._id
+                }
+               
+            })
+        })
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    } );
+});
+
+router.delete("/:userid", (req, res, next) => {
+    User.remove({_id: req.params.userid})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "User deleted"
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+});
+
+router.patch("/:userid", (req, res, next) => {
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    User.updateOne({_id: req.params.userid}, {$set: updateOps})
+        .exec()
+        .then(user => {
+            res.status(200).json({
+                message: "User updated successfully"
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+})
+
+module.exports = router;
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/user");
 
 mongoose.connect("mongodb+srv://admin:"
                  +process.env.MONGO_ATLAS_PW+ 
@@ -32,6 +33,8 @@ app.use((req, res, next) => {
 //routes
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
+
 
 
 app.use((req, res, next) => {
